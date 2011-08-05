89ed3f0076349196a44dcc39a8d6f570602667f0
Graham Scott <ghound@gmail.com>
2011-08-05 11:45:21 +0200

    Refactored to use jquery plugin boilerplate pattern. The plugin is now applied via a selector $('.foo').placeholderShim() as previously it failed to apply correctly to hidden elements, so a single global application of the shim isn't always desirable.

--- jquery.html5-placeholder-shim.js
+++ jquery.html5-placeholder-shim.js
@@ -1,72 +1,89 @@
 (function($) {
-	$.extend($,{ placeholder: {
-			browser_supported: function() {
-				return this._supported !== undefined ?
-					this._supported :
-					( this._supported = !!('placeholder' in $('<input type="text">')[0]) );
-			},
-			shim: function(opts) {
-				var config = {
-					color: '#888',
-					cls: '',
-					lr_padding:4
-				};
-				$.extend(config,opts);
-				!this.browser_supported() && $('input[placeholder]')._placeholder_shim(config);
-			}
-	}});
+    $.fn.placeholderShim = function(method) {
 
-	$.extend($.fn,{
-		_placeholder_shim: function(config) {
-			function calcPositionCss(target)
-			{
-				var op = $(target).offsetParent().offset();
-				var ot = $(target).offset();
-				//console.log(op,ot);
-				return {
-					top: ot.top - op.top + ($(target).outerHeight() - $(target).height()) /2 + $(target).height()*.07,
-					left: ot.left - op.left + config.lr_padding,
-					width: $(target).width() - config.lr_padding
-				};
-			}
-			return this.each(function() {
-				if( $(this).data('placeholder') )
-					return true;
-				
-				var ol = $('<label />')
-					.text($(this).attr('placeholder'))
-					.addClass(config.cls)
-					.css({ 
-						position:'absolute', 
-						display: 'inline',
-						float:'none',
-						overflow:'hidden', 
-						whiteSpace:'nowrap',
-						textAlign: 'left',
-						color: config.color, 
-						cursor: 'text',
-						fontSize: parseInt($(this).height() * .85)
-					})
-					.css(calcPositionCss(this))
-					.data('target',$(this))
-					.click(function(){
-						$(this).data('target').focus()
-					})
-					.insertAfter(this);
-				$(this)
-					.data('placeholder',ol)
-					.focus(function(){
-						ol.hide();
-					}).blur(function() {
-						ol[$(this).val().length ? 'hide' : 'show']();
-					}).triggerHandler('blur');
-				$(window)
-					.resize(function() {
-						var $target = ol.data('target')
-						ol.css(calcPositionCss($target))
-					});
-			});
-		} 
-	});
-	
+        var methods = {
+
+            init : function(options) {
+                alert('foooo');
+                var settings = this.placeholderShim.settings = $.extend({}, this.placeholderShim.defaults, options);
+                return this.each(function() {
+                    var $element = $(this),
+                         element = this;
+                    
+                    if($element.data('placeholder')){
+                        return true;
+                    }
+        
+                    var ol = $('<label />');
+
+                    ol.text($element.attr('placeholder'));
+                    ol.addClass(settings.cls);
+                            ol.css({ 
+                                position:'absolute', 
+                                display: 'inline',
+                                float:'none',
+                                overflow:'hidden', 
+                                whiteSpace:'nowrap',
+                                textAlign: 'left',
+                                color: settings.color, 
+                                cursor: 'text',
+                                fontSize: parseInt($element.height() * .85)
+                            });
+                            ol.css(helpers.calcPositionCss(element, settings));
+                            ol.data('target', $element);
+                            ol.click(function(){
+                                $element.data('target').focus()
+                            });
+                            ol.insertAfter($element);
+                    
+                    $element
+                        .data('placeholder',ol)
+                        .focus(function(){
+                            ol.hide();
+                        })
+                        .blur(function() {
+                            ol[$element.val().length ? 'hide' : 'show']();
+                            })
+                        .triggerHandler('blur');
+                    
+                    $(window).resize(function() {
+                        var $target = ol.data('target');
+                        ol.css(helpers.calcPositionCss($target, settings));
+                    });
+                });
+            }
+        }
+
+        var helpers = {
+            calcPositionCss: function(target, settings)
+            {
+                var op = $(target).offsetParent().offset(),
+                    ot = $(target).offset();
+                
+                return {
+                    top: ot.top - op.top + ($(target).outerHeight() - $(target).height()) /2 + $(target).height()*.07,
+                    left: ot.left - op.left + settings.lr_padding,
+                    width: $(target).width() - settings.lr_padding
+                };
+            }
+        }
+
+        if (methods[method]) {
+            return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+        } else if (typeof method === 'object' || !method) {
+            return methods.init.apply(this, arguments);
+        } else {
+            $.error( 'Method "' +  method + '" does not exist in placeholderShim plugin!');
+        }
+
+    }
+
+    $.fn.placeholderShim.defaults = {
+        color: '#888',
+        cls: 'placeholder_shim',
+        lr_padding:4
+    }
+
+    $.fn.placeholderShim.settings = {}
+
 })(jQuery);
